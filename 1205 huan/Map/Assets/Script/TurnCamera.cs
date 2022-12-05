eb795ec789d195ffbddcc36fbe9e6689bccb5479
Josh Chen <josh20010626@gmail.com>
2022-12-05 23:56:28 +0800

    1205 huan

---
+++ 1205 huan/Map/Assets/Script/TurnCamera.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class TurnCamera : MonoBehaviour
+{
+    public GameObject player;
+    public CharacterController controller;
+    public Camera camera;
+    public ThirdPersonMovement thirdPersonMovement;
+    public CinemachineFreeLook freelook;
+    public GoToSpinningRoom2 goToSpinningRoom2;
+
+    private float spinningAngle = 0;
+    private float spinAmount = 1.5f;
+    private float spinningAnglePre = 0;
+    public bool flag = true;
+
+    private float speedMod = 5.0f;      //a speed modifier
+    private Vector3 point;              //the coord to the point where the camera looks at
+    private float angleEnd = 0f;
+
+    private float angleInitial;
+    private float desiredAngle;
+    
+    public float upperAngle = 720.0f;
+    public float lowerAngle = 180.0f;
+
+    public float timer = 0f;
+
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        
+        
+        
+        
+        if (goToSpinningRoom2.isSpinning)
+        {
+            
+            if (flag == true)
+            {
+                if (timer < 0.5f)
+                {
+                    timer += Time.deltaTime;
+                    return;
+                }
+                else
+                {
+                    timer = 0;
+                }
+                camera.GetComponent<CinemachineBrain>().enabled = false;
+                angleInitial = transform.rotation.eulerAngles.y;
+                point = player.transform.position;      // get target's coords
+                transform.LookAt(point);                // makes the camera look to it
+                flag = false;
+                desiredAngle = Random.Range(lowerAngle, upperAngle);
+                
+            }
+
+            if (angleEnd < desiredAngle)
+            {
+                controller.transform.rotation = Quaternion.Euler(0f, angleInitial + angleEnd, 0f);              // absolutely
+                transform.RotateAround(point, new Vector3(0.0f, 1.0f, 0.0f), 20 * Time.deltaTime * speedMod);   // relatively
+                angleEnd += 20 * Time.deltaTime * speedMod;
+            }
+            else
+            {
+                freelook.m_Heading.m_Bias += desiredAngle;
+                flag = true;
+                while (freelook.m_Heading.m_Bias > 180)
+                {
+                    freelook.m_Heading.m_Bias -= 360;
+                }
+                while (freelook.m_Heading.m_Bias < -180)
+                {
+                    freelook.m_Heading.m_Bias += 360;
+                }
+
+                controller.enabled = true;
+                goToSpinningRoom2.lockMouse = false;
+                camera.GetComponent<CinemachineBrain>().enabled = true;
+                goToSpinningRoom2.isSpinning = false;
+                angleEnd = 0;
+                
+            }
+        }        
+    }
+
+}
